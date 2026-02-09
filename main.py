e3ee6313df1ded20490900039012a026e55521a3
Srijan Suresh <srijansuresh04@gmail.com>
2026-02-09 11:08:28 -0600

    added TUI

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import sys, time
+from rich.live import Live
+from rich.table import Table
 from sentinel_pkg.guardian import Guardian
 from sentinel_pkg.ipc import IPCServer
+
+def generate_dashboard(guardian):
+    # Generate a real-time table UI
+    table = Table(title="Sentinel: System Guardian", show_header=True, header_style="bold magenta")
+    table.add_column("PID", style="dim", width=12)
+    table.add_column("Status", justify="center")
+    table.add_column("Memory Limit", justify ="right")
+
+    # fetching current status
+    status = guardian.get_status()
+    pid = str(guardian.process.pid) if guardian.process else "N/A"
+
+    table.add_row(pid, status, f"[green]{guardian.limit} MB[/green]")
+    return table
 
 def main():
     # we skip our script name and just get the command
@@ -17,26 +33,30 @@ def main():
 
     guardian.start()
     server.start()
-    print(f"Server active. Montoring PID: {guardian.process.pid}")
+
     # Manage Process while it's running
-    try:
-        while not guardian.poll():
-            # checking incoming commands (IPC)
-            conn, msg = server.check_for_client()
-            if msg:
-                if msg == "status":
-                    res = f"Guardian: {guardian.get_status()}\n"
-                    conn.sendall(res.encode())
-                elif msg == "stop":
-                    guardian.kill()
-                    break
-                conn.close() # we close our line after msg
-            # lets verify our mem-usage here
-            guardian.check_resources()
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("\nSentinel shutting down..")
-        guardian.kill()
+    with Live(generate_dashboard(guardian), refresh_per_second=4) as live:
+        try:
+            while not guardian.poll():
+                # checking incoming commands (IPC)
+                conn, msg = server.check_for_client()
+                if msg:
+                    if msg == "status":
+                        res = f"Guardian: {guardian.get_status()}\n"
+                        conn.sendall(res.encode())
+                    elif msg == "stop":
+                        guardian.kill()
+                        break
+                    conn.close() # we close our line after msg
+                # lets verify our mem-usage here
+                guardian.check_resources()
+                # update dashboard
+                live.update(generate_dashboard(guardian))
+                time.sleep(0.5)
+        except KeyboardInterrupt:
+            print("\nSentinel shutting down..")
+            guardian.kill()
+
     print(f"Final report:{guardian.get_status()}")
 
 if __name__ == '__main__':
